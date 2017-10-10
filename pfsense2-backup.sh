3dac96267761bf841951ca1de1cb8bef1fa85500
Ben Tyger <btyger@syferlock.com>
2017-10-10 13:23:44 -0400

    Cleaned up diff merges issues

--- pfsense2-backup.sh
+++ pfsense2-backup.sh
@@ -39,22 +39,6 @@ function clean_up {
 	fi
 }
 
-<<<<<<< HEAD
-=======
-if [ ! -z "${TMP_POSTDATA_FILE}" ] && [ -e "${TMP_POSTDATA_FILE}" ]; then
-	logger -p user.debug -t "${APPNAME}" -- "Removing sensitive file ${TMP_POSTDATA_FILE}"
-	rm "${TMP_POSTDATA_FILE}"
-fi
-	
-
-if [ -e "${TMPAUTHFILE}" ] ; then 
-	logger -p user.debug -t "${APPNAME}" -- "Removing sensitive file  ${TMPAUTHFILE}" 
-	rm "${TMPAUTHFILE}"
-fi
-return 0
-}
-
->>>>>>> c117e2df2571981da572cb710882580369ca8546
 
 function get_csrf {
 	local PAGE_FILE="${1}"
@@ -63,14 +47,7 @@ function get_csrf {
 		echo "Could not read ${PAGE_FILE}" 1>&2
 		return 2
 	fi 
-<<<<<<< HEAD
-	CSRF_VALUE=$(grep 'var csrfMagicToken = "' "${PAGE_FILE}" |sed -r 's/.*(sid\:[0-9a-f]+).*/\1/')
-	if [ $? -ne 0 ] ; then
-		echo "Failed to get parse CSRF out of HTML file." 1>&2
-		return 3
-	fi
-	echo "$CSRF_VALUE"
-=======
+
 	CSRF_GREP=$(grep 'var csrfMagicToken = "' "${PAGE_FILE}") 
 	if [ $? -ne 0 ] ; then
 		echo "Failed to get grep CSRF out of HTML file." 1>&2
@@ -82,7 +59,7 @@ function get_csrf {
 		return 4
 	fi
 	echo "${CSRF_VALUE}"
->>>>>>> c117e2df2571981da572cb710882580369ca8546
+
 	return 0
 }
 
@@ -161,7 +138,6 @@ if [ "${IGNORE_UNTRUSTED_CERTIFICATES,,}" == "true" ] ; then
 else 
 	IGNORE_UNTRUSTED_CERTIFICAT_SET=''	 
 fi
-<<<<<<< HEAD
 
 ## Get Login page
 PAGE_OUTPUT=$(mktemp)
@@ -201,7 +177,6 @@ URL_CSRF="$(perl -MURI::Escape -e 'print url_escape($ARGV[0]);' "${CSRF")"
 TMPAUTHFILE="$(mktemp)"
 echo -n "login=Login&usernamefld=${URLUSER}&passwordfld=${URLPASS}&__csrf_magic=${URL_CSRF}" > ${TMPAUTHFILE}
 
-=======
 
 ## Get Login page
 PAGE_OUTPUT=$(mktemp)
@@ -241,22 +216,12 @@ URL_CSRF="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${CSRF}")"
 TMPAUTHFILE="$(mktemp)"
 echo -n "login=Login&usernamefld=${URL_USER}&passwordfld=${URL_PASS}&__csrf_magic=${URL_CSRF}" > ${TMPAUTHFILE}
 
->>>>>>> c117e2df2571981da572cb710882580369ca8546
 PAGE_OUTPUT=$(mktemp)
 logger -p user.debug -t "${APPNAME}" -- "Submitting login credentials"
 wget \
   --keep-session-cookies \
   --load-cookies ${COOKIEFILE} \
   --save-cookies ${COOKIEFILE} \
-<<<<<<< HEAD
-  "${IGNORE_UNTRUSTED_CERTIFICAT_SET}" \
-  -O "${PAGE_OUTPUT}" \
-  --post-file "${TMPAUTHFILE}" \
-  "https://${PFSHOSTNAME}/index.php" 2>&1 1>/dev/null 
-LOGINRES=$?
-rm "${TMPAUTHFILE}"
-if [ ${LOGINRES} -ne 0 ] ; then 
-=======
   ${IGNORE_UNTRUSTED_CERTIFICAT_SET} \
   -O "${PAGE_OUTPUT}" \
   --post-file "${TMPAUTHFILE}" \
@@ -264,7 +229,6 @@ if [ ${LOGINRES} -ne 0 ] ; then
 LOGIN_RES=$?
 rm "${TMPAUTHFILE}"
 if [ ${LOGIN_RES} -eq 0 ] ; then 
->>>>>>> c117e2df2571981da572cb710882580369ca8546
 	logger -p user.debug -t "${APPNAME}" -- "Successfully logged in to pfSense(${PFSHOSTNAME})"
 else 
 	logger -p user.error -s -t "${APPNAME}" -- "Failed to logged in to pfSense(${PFSHOSTNAME})"
@@ -281,7 +245,6 @@ if [ $CSRF_RET -ne 0 ] ; then
 	exit 2
 fi
 
-<<<<<<< HEAD
 CSRF=$(get_csrf ${PAGE_OUTPUT})
 CSRF_RET=?
 rm "${PAGE_OUTPUT}"
@@ -294,37 +257,18 @@ fi
 ##########################
 ### Downloading the Config
 ##########################
-
-POSTDATA='Submit=download'
-URL_CSRF="$(perl -MURI::Escape -e 'print url_escape($ARGV[0]);' "${CSRF")"
-POSTDATA="${POSTDATA}&_csrf_magic=${URL_CSRF}"
-if ! ${BACKUPRRD} ; then 
-=======
-##########################
-### Downloading the Config
-##########################
 DOWNLOAD_VALUE="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "Download configuration as XML")"
 POSTDATA="download=${DOWNLOAD_VALUE}"
 URL_CSRF="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${CSRF}")"
 POSTDATA="${POSTDATA}&__csrf_magic=${URL_CSRF}"
 if [ "${BACKUPRRD,,}" == "true" ] ; then
 	logger -p user.debug -t "${APPNAME}" -- "Enabled RRD backups" 
->>>>>>> c117e2df2571981da572cb710882580369ca8546
+
 	POSTDATA="${POSTDATA}&donotbackuprrd=on"
 fi 
 if [ ! -z "${ENCRYPTPASS}" ] ; then
 	logger -p user.debug -t "${APPNAME}" -- "Encrypting backup"
 	URLENCRYPTPASS="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${ENCRYPTPASS}")"
-<<<<<<< HEAD
-	POSTDATA="${POSTDATA}&encrypt=on&encrypt_password=${URLENCRYPTPASS}&encrypt_passconf=${URLENCRYPTPASS}"
-fi
-
-### Writing POST DATA to file
-TMP_POSTDATA_FILE=$(mktemp)
-echo "${POSTDATA}" | tee  ${TMP_POSTDATA_FILE} 1>/dev/null
-if [ $? -ne 0 ] ; then
-	logger -p user.errr -s -t "${APPNAME}" -- "Failed to write POST data to temp file"
-=======
 	POSTDATA="${POSTDATA}&encrypt=on&encrypt_password=${URLENCRYPTPASS}&encrypt_passconf=${URLENCRYPTPASS}&"
 fi
 if [ "${BACKUP_PACKAGES,,}" == "true" ] ;then 
@@ -332,30 +276,16 @@ if [ "${BACKUP_PACKAGES,,}" == "true" ] ;then
 	POSTDATA="${POSTDATA}&nopackages=yes"
 fi
 
-POSTDATA_FILE=$(mktemp)
-echo "${POSTDATA}" > "${POSTDATA_FILE}"
-
 ### Writing POST DATA to file
-TMP_POSTDATA_FILE=$(mktemp)
-echo "${POSTDATA}" | tee  ${TMP_POSTDATA_FILE} 
+POSTDATA_FILE=$(mktemp)
+echo "${POSTDATA}" | tee  ${POSTDATA_FILE} 
 if [ $? -ne 0 ] ; then
 	logger -p user.error -s -t "${APPNAME}" -- "Failed to write POST data to temp file"
->>>>>>> c117e2df2571981da572cb710882580369ca8546
 	exit 2
 fi   
 
 ## Getting backup file over HTTPS
 
-<<<<<<< HEAD
-wget 
-  --keep-session-cookies \
-  --load-cookies ${COOKIEFILE} \
-  --save-cookies ${COOKIEFILE} \
-  "${IGNORE_UNTRUSTED_CERTIFICAT_SET}" \
-  -O "${BACKUPDIR}/${BACKUPFILE}" \
-  --post-data "${POSTDATA}" \
-  "https://${PFSHOSTNAME}/diag_backup.php" 2>&1 1>/dev/null 
-=======
 wget \
   --keep-session-cookies \
   --load-cookies ${COOKIEFILE} \
@@ -364,7 +294,7 @@ wget \
   -O "${BACKUPDIR}/${BACKUPFILE}" \
   --post-file="${POSTDATA_FILE}" \
   "https://${PFSHOSTNAME}/diag_backup.php" 
->>>>>>> c117e2df2571981da572cb710882580369ca8546
+
    
 BACKUPRES=$?
 rm "${POSTDATA_FILE}"
